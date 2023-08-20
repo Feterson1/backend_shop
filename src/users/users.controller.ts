fcd8b36bf7ec01f97832854bfee62ae914bc2aa4
Feterson <feterson@bk.ru>
2023-08-20 20:54:29 +0600

    create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpStatus, Header, HttpCode } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService){}
+
+    @Post('/signup')
+    @HttpCode(HttpStatus.CREATED)
+    @Header('Content-type','application/json')
+    createUser(@Body() createUserDto:CreateUserDto){
+        return this.usersService.create(createUserDto);
+    }
+
+}
